9a78ec9fd3533fc5aa967dbaca5b27a0ad0024c2
Sean Lee <leejoneshane@gmail.com>
2019-05-22 14:45:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,8 @@
 
 cd /makecode/pxt-microbit
 
-#if [ ! -z "$*" ]; then
-#    pxt $*
-#else
-#    pxt serve -h 0.0.0.0 -p 80 --noBrowser
-#fi
-
-exec "$*"
+if [ ! -z "$*" ]; then
+    /makecode/pxt/pxt-cli/pxt $*
+else
+    /makecode/pxt/pxt-cli/pxt serve -h 0.0.0.0 -p 80 --noBrowser
+fi
